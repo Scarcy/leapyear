f0175e9a2f910427f0638aa1325c90fd596844cc
Ellefsen <monsoyen@gmail.com>
2022-10-19 17:25:15 +0200

    initial commit

---
+++ src/main/java/LeapYear.java
@@ -0,0 +1,11 @@
+public class LeapYear {
+
+    public boolean isLeapYear(int year){
+       if(year % 4 == 0 && year % 100 != 0){
+           return true;
+       }
+       else return year % 400 == 0;
+    }
+
+
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+
+       // LeapYear leap = new LeapYear();
+    }
+
+}
